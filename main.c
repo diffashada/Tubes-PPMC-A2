717373a13ce675d63562db09c44c91a7073828a3
abbeljatmiko <93469927+abbeljatmiko@users.noreply.github.com>
2024-05-15 11:42:09 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+char maze[MAX_SIZE][MAX_SIZE];
+bool visited[MAX_SIZE][MAX_SIZE];
+int nRows, nCols;
+
+int main() {
+    char filename[100];
+    printf("Enter the maze file name: ");
+    scanf("%s", filename);
+    readMaze(filename);
+
+    int startX = -1, startY = -1, endX = -1, endY = -1;
+
+    // Mencari titik S dan E
+    for (int i = 0; i < nRows; i++) {
+        for (int j = 0; j < nCols; j++) {
+            if (maze[i][j] == 'S') {
+                startX = i;
+                startY = j;
+            } else if (maze[i][j] == 'E') {
+                endX = i;
+                endY = j;
+            }
+        }
+    }
+
+    if (startX == -1 || startY == -1 || endX == -1 || endY == -1) {
+        printf("Start or end not found in the maze.\n");
+        return 1;
+    }
+
+    int path[MAX_SIZE * MAX_SIZE][2]; 
+    int *shortestPath = malloc(MAX_SIZE * MAX_SIZE * 2 * sizeof(int));
+    int *longestPath = malloc(MAX_SIZE * MAX_SIZE * 2 * sizeof(int));
+    int shortestLength = 0, longestLength = 0;
+
+    DFS(startX, startY, endX, endY, path, 0, &shortestPath, &longestPath, &shortestLength, &longestLength);
+
+    printf("Shortest Path: ");
+    for (int i = 0; i < shortestLength; i++) {
+        printf("(%d,%d)", shortestPath[i*2] + 1, shortestPath[i*2 + 1] + 1);
+        if (i < shortestLength - 1) printf(" -> ");
+    }
+    printf("\n");
+
+    printf("Longest Path: ");
+    for (int i = 0; i < longestLength; i++) {
+        printf("(%d,%d)", longestPath[i*2] + 1, longestPath[i*2 + 1] + 1);
+        if (i < longestLength - 1) printf(" -> ");
+    }
+    printf("\n");
+
+    free(shortestPath);
+    free(longestPath);
+
+    return 0;
+}

96983e617fba796533b479c1056e28136a7c6baf
diffashada <82130163+diffashada@users.noreply.github.com>
2024-05-19 10:43:41 +0700

    Update Backtracking.c

--- Backtracking.c
+++ Backtracking.c
@@ -1,6 +1,3 @@
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -11,41 +8,53 @@
 char maze[MAX_SIZE][MAX_SIZE];
 bool visited[MAX_SIZE][MAX_SIZE];
 int nRows, nCols;
-int shortestLength = 0, longestLength = 0;
-int shortestPath[MAX_SIZE * MAX_SIZE][2], longestPath[MAX_SIZE * MAX_SIZE][2];
 
 // Menentukan apakah sel adalah valid untuk dilewati
 bool isValid(int x, int y) {
     return x >= 0 && x < nRows && y >= 0 && y < nCols && maze[x][y] != '#' && !visited[x][y];
 }
 
-// Backtracking untuk mencari semua jalur yang mungkin
-void backtracking(int x, int y, int endX, int endY, int path[][2], int pathIndex) {
+// Fungsi untuk mencetak path
+void printPath(int path[][2], int pathLength, int **shortestPath, int **longestPath, int *shortestLength, int *longestLength) {
+    printf("Path: ");
+    for (int i = 0; i < pathLength; i++) {
+        printf("(%d,%d)", path[i][0] + 1, path[i][1] + 1);
+        if (i < pathLength - 1) printf(" -> ");
+    }
+    printf("\n");
+
+    // Update shortest path
+    if (*shortestLength == 0 || pathLength < *shortestLength) {
+        *shortestLength = pathLength;
+        memcpy(*shortestPath, path, pathLength * 2 * sizeof(int));
+    }
+
+    // Update longest path
+    if (pathLength > *longestLength) {
+        *longestLength = pathLength;
+        memcpy(*longestPath, path, pathLength * 2 * sizeof(int));
+    }
+}
+
+// Backtracking function to find all possible paths
+void solveMazeBacktracking(int x, int y, int endX, int endY, int path[][2], int pathIndex, int **shortestPath, int **longestPath, int *shortestLength, int *longestLength) {
     visited[x][y] = true;
     path[pathIndex][0] = x;
     path[pathIndex][1] = y;
     pathIndex++;
 
     if (x == endX && y == endY) {
-        // Update shortest path
-        if (shortestLength == 0 || pathIndex < shortestLength) {
-            shortestLength = pathIndex;
-            memcpy(shortestPath, path, pathIndex * 2 * sizeof(int));
-        }
-        // Update longest path
-        if (pathIndex > longestLength) {
-            longestLength = pathIndex;
-            memcpy(longestPath, path, pathIndex * 2 * sizeof(int));
-        }
+        printPath(path, pathIndex, shortestPath, longestPath, shortestLength, longestLength);
     } else {
-        // Explore atas, bawah, kiri, kanan
-        if (isValid(x + 1, y)) backtracking(x + 1, y, endX, endY, path, pathIndex);
-        if (isValid(x - 1, y)) backtracking(x - 1, y, endX, endY, path, pathIndex);
-        if (isValid(x, y + 1)) backtracking(x, y + 1, endX, endY, path, pathIndex);
-        if (isValid(x, y - 1)) backtracking(x, y - 1, endX, endY, path, pathIndex);
+        // Explore down, up, right, left
+        if (isValid(x + 1, y)) solveMazeBacktracking(x + 1, y, endX, endY, path, pathIndex, shortestPath, longestPath, shortestLength, longestLength);
+        if (isValid(x - 1, y)) solveMazeBacktracking(x - 1, y, endX, endY, path, pathIndex, shortestPath, longestPath, shortestLength, longestLength);
+        if (isValid(x, y + 1)) solveMazeBacktracking(x, y + 1, endX, endY, path, pathIndex, shortestPath, longestPath, shortestLength, longestLength);
+        if (isValid(x, y - 1)) solveMazeBacktracking(x, y - 1, endX, endY, path, pathIndex, shortestPath, longestPath, shortestLength, longestLength);
     }
 
     visited[x][y] = false;
+    pathIndex--;
 }
 
 // Baca input file
@@ -93,22 +102,28 @@ int main() {
     }
 
     int path[MAX_SIZE * MAX_SIZE][2]; 
+    int *shortestPath = malloc(MAX_SIZE * MAX_SIZE * 2 * sizeof(int));
+    int *longestPath = malloc(MAX_SIZE * MAX_SIZE * 2 * sizeof(int));
+    int shortestLength = 0, longestLength = 0;
 
-    backtracking(startX, startY, endX, endY, path, 0);
+    solveMazeBacktracking(startX, startY, endX, endY, path, 0, &shortestPath, &longestPath, &shortestLength, &longestLength);
 
     printf("Shortest Path: ");
     for (int i = 0; i < shortestLength; i++) {
-        printf("(%d,%d)", shortestPath[i][0] + 1, shortestPath[i][1] + 1);
+        printf("(%d,%d)", shortestPath[i*2] + 1, shortestPath[i*2 + 1] + 1);
         if (i < shortestLength - 1) printf(" -> ");
     }
     printf("\n");
 
     printf("Longest Path: ");
     for (int i = 0; i < longestLength; i++) {
-        printf("(%d,%d)", longestPath[i][0] + 1, longestPath[i][1] + 1);
+        printf("(%d,%d)", longestPath[i*2] + 1, longestPath[i*2 + 1] + 1);
         if (i < longestLength - 1) printf(" -> ");
     }
     printf("\n");
+
+    free(shortestPath);
+    free(longestPath);
 
     return 0;
 }
